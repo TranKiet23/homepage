05461b18306c94c397300bd027f0e7cc8275c1e0
KietTran <kiet.tran@mekship.com>
2024-05-28 18:08:35 +0700

    update

--- components/CarouselHome.vue
+++ components/CarouselHome.vue
@@ -1,5 +1,22 @@
 <template>
     <div id="home-top" class="container mx-auto flex flex-wrap items-center">
+        <div class="w-full" style="width: 100%; display: inline-block;">
+            <div class="flex flex-wrap items-center w-full">
+                <div class="w-full sm:w-1/2 flex flex-col justify-center sm:pr-24">
+                    <h1 class="number-font font-bold text-left">MEKWMS WAREHOUSE MANAGEMENT SOLUTIONS</h1>
+                    <div class="flex w-full mt-5 lg:flex-no-wrap flex-wrap">
+                        <div class="flex text-center w-full lg:w-1/2 lg:pr-3"><button
+                                class="btn-white-text-blue modal-open lg:mb-0 mb-3 w-full" tabindex="0">Try
+                                Demo</button></div>
+                        <div class="flex text-center w-full lg:w-1/2 lg:pl-3"><a
+                                href="https://meksmart.com/en/solutions" class="btn-blue cursor-pointer w-full"
+                                tabindex="0">Read more</a></div>
+                    </div>
+                </div>
+                <div class="w-full sm:w-1/2 flex justify-end items-center pl-6 sm:pr-0 pr-6 py-10"><img
+                        src="https://meksmart.com/storage/shares/Hinh Anh/Hình present-35.png"></div>
+            </div>
+        </div>
         <div class="slick-event w-full slick-big slick-initialized slick-slider slick-dotted">
             <button type="button" class="slick-prev pull-left slick-arrow" style=""><img
                     src="https://meksmart.com/images/web/icon/element-09.svg" class="blue-icon"><img
