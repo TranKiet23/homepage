59b0f82d2f6923e1ec7979e26a20ac13d7af3a22
KietTran <kiet.tran@mekship.com>
2024-05-29 12:05:38 +0700

    update

---
+++ pages/HomePageManage.vue
@@ -0,0 +1,138 @@
+<template>
+    <section id="quan-li" class="relative">
+        <div class="w-full relative">
+            <div class="container absolute h-full top-0" style="padding: 0px; min-width: 70vw;">
+                <div
+                    class="bg-gradient-rl py-10 w-full h-full pl-20 pr-10 border-radius-right justify-end flex items-center flex-wrap relative">
+                    <div class="lg:w-1/3 md:w-1/3 sm:w-1/2 w-full absolute md:flex hidden z-40 r-18">
+                        <div id="tieu-chuan" class="flex flex-wrap items-center my-3 raleway-bold"><a
+                                href="http://meksmart.com/en/solutions/mek-wms"
+                                class="w-full items-end flex flex-row btn-rounded-top btn-content">
+                                <div class="number-font font-bold flex items-center"> <img
+                                        src="https://meksmart.com/images/web/icon/to-right-blue.svg" class="icon-blue">
+                                    <div>MEKWMS</div>
+                                </div>
+                            </a> <a href="http://meksmart.com/en/solutions/mek-wms-3pls"
+                                class="w-full items-end flex flex-row btn-rounded-center btn-content">
+                                <div class="number-font font-bold flex items-center"> <img
+                                        src="https://meksmart.com/images/web/icon/to-right-blue.svg" class="icon-blue">
+                                    <div>MEKTMS</div>
+                                </div>
+                            </a> <a href="https://meksmart.com/en/solutions/mek-wms-theo-yeu-cau"
+                                class="w-full items-end flex flex-row btn-rounded-bottom btn-content">
+                                <div class="number-font font-bold flex items-center"> <img
+                                        src="https://meksmart.com/images/web/icon/to-right-blue.svg" class="icon-blue">
+                                    <div>MEKSMART CONSULTING, UPGRADE</div>
+                                </div>
+                            </a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="container mx-auto relative lg:z-30">
+                <div class=" py-6 w-full pr-10 border-radius-right flex items-center flex-wrap">
+                    <div class="lg:w-2/5 md:w-1/2 w-full flex justify-center">
+                        <div class="w-full flex flex-col items-center">
+                            <h2 class="text-white raleway-bold">
+                                <p style="text-align: center;"><span
+                                        style="font-size: 24pt; color: rgb(255, 255, 255);"><strong>MEKSMART</strong></span><br><span
+                                        style="font-size: 24pt; color: rgb(255, 255, 255);"><strong>TECHNOLOGY AND
+                                            SOLUTIONS</strong></span></p>
+                            </h2> <img src="https://meksmart.com/images/web/home/icon-blue.png"
+                                class="py-4 lg:px-10 sm:px-6 px-4">
+                            <div class="w-full px-4 flex justify-center"><img
+                                    src="https://meksmart.com/images/web/icon/three-white.png" style="width: 150px;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full md:hidden flex">
+                        <div id="tieu-chuan" class="flex flex-wrap items-center my-3 raleway-bold md:w-1/2 w-full"><a
+                                href="http://meksmart.com/en/solutions/mek-wms"
+                                class="w-full justify-end flex flex-col btn-rounded-top btn-content">
+                                <div>MEKWMS</div>
+                                <img src="https://meksmart.com/images/web/icon/to-right-blue.svg" class="icon-blue">
+                            </a> <a href="http://meksmart.com/en/solutions/mek-wms-3pls"
+                                class="w-full justify-end flex flex-col btn-rounded-center btn-content">
+                                <div>MEKTMS</div>
+                                <img src="https://meksmart.com/images/web/icon/to-right-blue.svg" class="icon-blue">
+                            </a> <a href="https://meksmart.com/en/solutions/mek-wms-theo-yeu-cau"
+                                class="w-full justify-end flex flex-col btn-rounded-bottom btn-content">
+                                <div>MEKSMART CONSULTING, UPGRADE</div>
+                                <img src="https://meksmart.com/images/web/icon/to-right-blue.svg" class="icon-blue">
+                            </a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lg:pb-48 pb-24 bg-quat-bottom relative"></div>
+    </section>
+</template>
+
+<style lang="scss">
+.border-radius-right {
+    padding-top: 30px;
+    padding-bottom: 30px;
+    border-bottom-right-radius: 95px;
+    border-top-right-radius: 95px;
+}
+
+.bg-gradient-rl {
+    color: #fff;
+    background-image: linear-gradient(270deg, #0521bf, #001491);
+}
+
+.btn-content {
+    font-family: Raleway-Bold;
+    font-size: 18px;
+    cursor: pointer;
+    color: #fff;
+
+    .icon-blue {
+        width: 20px;
+        display: block;
+        margin-bottom: 4px;
+        margin-right: 3px;
+    }
+}
+
+.btn-rounded-top {
+    padding: 30px 40px;
+    margin-bottom: 10px;
+    background-image: url(https://meksmart.com/images/button-blue-top.png?e02922bd841e787bd14df0310e40a1a6);
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: 100% 100%;
+    min-height: 180px;
+    &:hover {
+    background-image: url(https://meksmart.com/images/button-orange-top.png?9ec5e2560a7bc18a6d0f5d3dc62893fa);
+    color: #071699;
+    }
+}
+
+.btn-rounded-center {
+    padding: 30px 40px;
+    margin-bottom: 10px;
+    background-image: url(https://meksmart.com/images/button-blue-center.png?63ebab5d714d9974a25a57202436e3e6);
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: 100% 100%;
+    min-height: 180px;
+    &:hover {
+        background-image: url(https://meksmart.com/images/button-orange-center.png?81a0aff0959e66b356bf4a805a36031c);
+        color: #071699;
+    }
+}
+
+.btn-rounded-bottom {
+    background-image: url(https://meksmart.com/images/button-blue-bottom.png?5ef29b4c46c1a30715885ecc57ce795c);
+    padding: 30px 40px;
+    margin-bottom: 10px;
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: 100% 100%;
+    min-height: 180px;
+    &:hover {
+    background-image: url(https://meksmart.com/images/button-orange-bottom.png?44e33d2ca2c935a6f010daa2d26717b6);
+    color: #071699;
+    }
+}
+</style>
---
+++ pages/HomePageReadMore.vue
@@ -0,0 +1,28 @@
+<template>
+    <section id="den-voi-meksmart" class="bg-gray-light bg-image-rcl">
+        <div class="container mx-auto pt-8 pb-32">
+            <div class="flex flex-wrap items-center">
+                <div class="small-content w-full lg:w-1/2 lg:pr-6">
+                    <h2 class="raleway-bold"></h2>
+                    <h1 class="raleway-bold" style="line-height: 1.1;"><span
+                            style="font-size: 36pt; color: rgb(0, 20, 145);">Meksmart</span></h1>
+                    <h4 class="raleway-bold mb-4"></h4>
+                    <h1 class="raleway-bold" style="line-height: 1.1;"><span
+                            style="font-size: 18pt; color: rgb(0, 20, 145);">Not only providing software<br>We have the
+                            best solutions</span></h1>
+                    <p>&nbsp;</p>
+                    <p><span style="font-size: 14pt;">Meksmart puts the customer's success factor on top. By integrating
+                            people, experience, expertise, support, combined with a smart and modern technology
+                            platform, Meksmart is committed to providing comprehensive solutions for enterprises'
+                            logistics activities. Replace manual, complicated and inefficient logistics
+                            operations.</span></p>
+                    <div class="flex mt-12 pb-10 pr-24 text-center w-full"><a href="https://meksmart.com/vi/about"
+                            class="btn-blue cursor-pointer sm:w-1/2 sm:pr-24">Read more</a></div>
+                </div>
+                <div class="image-content w-full lg:w-1/2 lg:pl-6"><img
+                        src="https://meksmart.com/storage/shares/IMG_HOME/home-hero-1.svg" alt="image intro"
+                        class="w-full"></div>
+            </div>
+        </div>
+    </section>
+</template>
