1a65a23d5df0726eea2f1a75c0513b6f792e0e58
KietTran <kiet.tran@mekship.com>
2024-05-30 09:36:44 +0700

    update

---
+++ pages/HomePageEvent.vue
@@ -0,0 +1,27 @@
+<template>
+    <section id="event" class="bg-event slick-big relative z-10">
+        <div class="container mx-auto relative">
+            <div class="next-top-orange"><img src="https://meksmart.com/images/web/icon/element-06.svg"></div>
+        </div>
+        <div class="w-full lg:pt-48 md:pt-20 pt-16 relative">
+            <div class="lg:w-2/5 sm:w-1/2 absolute bg-event-blue right-0 justify-end md:flex hidden"
+                style="padding: 0px;">
+                <div
+                    class="bg-gradient-lr py-10 w-95 h-full pl-20 pr-10 border-radius-left justify-end flex flex-wrap relative">
+                    <div class="w-full relative mt-4"><img src="https://meksmart.com/images/web/icon/element-07.svg" style="width: 150px;">
+                    </div>
+                </div>
+            </div>
+            <div class="lg:py-20 sm:pt-20 pt-10 pb-3">
+                <form action="https://meksmart.com/en/resource/search"
+                    class="relative mt-6 flex lg:flex-no-wrap flex-wrap">
+                    <div
+                        class="w-full sm:w-2/5 flex lg:flex-row flex-col pt-24 lg:items-start items-center lg:justify-center">
+                        <h2 class="raleway-bold fs-30 text-blue">News <br> And Events</h2>
+                    </div>
+            
+                </form>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ pages/HomePageFieldsLogistic.vue
@@ -0,0 +1,210 @@
+<template>
+    <section id="linh-vuc" class="w-full body-linh-vuc">
+        <div class="w-full radius-top-right py-6 bg-linh-vuc bg-white">
+            <div class="w-full bg-linh-vuc-top">
+                <div class="container mx-auto py-6 relative z-1">
+                    <h2 class="raleway-bold mt-6">
+                        <p><span style="font-size: 24pt; color: rgb(0, 20, 145);"><strong>FIELDS
+                                    LOGISTIC</strong></span></p>
+                    </h2>
+                    <div class="flex flex-wrap items-center py-6">
+                        <div class="lg:w-1/6 md:w-1/3 sm:w-1/2 w-full text-center"><a
+                                href="http://meksmart.com/en/domain/3pls"
+                                class="lg:mr-6 mb-6 p-6 h-full item-lv flex flex-col justify-center items-center">
+                                <div class="w-full flex" style="width: 90px; height: 90px;"><img
+                                        src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-01.svg"
+                                        style="width: 85px; height: auto;"></div> <span
+                                    class="number-font font-bold mt-1">3PLs</span>
+                            </a></div>
+                        <div class="lg:w-1/6 md:w-1/3 sm:w-1/2 w-full text-center"><a
+                                href="http://meksmart.com/en/domain/thuong-mai"
+                                class="lg:mr-6 mb-6 p-6 h-full item-lv flex flex-col justify-center items-center">
+                                <div class="w-full flex" style="width: 90px; height: 90px;"><img
+                                        src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-02.svg"
+                                        style="width: 85px; height: auto;"></div> <span
+                                    class="number-font font-bold mt-1">Commerces</span>
+                            </a></div>
+                        <div class="lg:w-1/6 md:w-1/3 sm:w-1/2 w-full text-center"><a
+                                href="http://meksmart.com/en/domain/phan-phoi"
+                                class="lg:mr-6 mb-6 p-6 h-full item-lv flex flex-col justify-center items-center">
+                                <div class="w-full flex" style="width: 90px; height: 90px;"><img
+                                        src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-03.svg"
+                                        style="width: 85px; height: auto;"></div> <span
+                                    class="number-font font-bold mt-1">Distributions</span>
+                            </a></div>
+                        <div class="lg:w-1/6 md:w-1/3 sm:w-1/2 w-full text-center"><a
+                                href="http://meksmart.com/en/domain/fmcg"
+                                class="lg:mr-6 mb-6 p-6 h-full item-lv flex flex-col justify-center items-center">
+                                <div class="w-full flex" style="width: 90px; height: 90px;"><img
+                                        src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-04.svg"
+                                        style="width: 85px; height: auto;"></div> <span
+                                    class="number-font font-bold mt-1">FMCG</span>
+                            </a></div>
+                        <div class="lg:w-1/6 md:w-1/3 sm:w-1/2 w-full text-center"><a
+                                href="http://meksmart.com/en/domain/san-xuat"
+                                class="lg:mr-6 mb-6 p-6 h-full item-lv flex flex-col justify-center items-center">
+                                <div class="w-full flex" style="width: 90px; height: 90px;"><img
+                                        src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-05.svg"
+                                        style="width: 85px; height: auto;"></div> <span
+                                    class="number-font font-bold mt-1">Manufactures</span>
+                            </a></div>
+                        <div class="lg:w-1/6 md:w-1/3 sm:w-1/2 w-full text-center"><a
+                                href="http://meksmart.com/en/domain/dich-vu"
+                                class="lg:mr-6 mb-6 p-6 h-full item-lv flex flex-col justify-center items-center">
+                                <div class="w-full flex" style="width: 90px; height: 90px;"><img
+                                        src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-06.svg"
+                                        style="width: 85px; height: auto;"></div> <span
+                                    class="number-font font-bold mt-1">Services</span>
+                            </a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- -------- plus --------- -->
+    <section id="3pls" class="bg-white">
+        <div class="container mx-auto pt-8 pb-8">
+            <div class="flex flex-wrap">
+                <div class="small-content w-full lg:w-1/2 lg:pr-6">
+                    <h2 class="number-font font-bold"></h2>
+                    <h1><span style="color: rgb(0, 20, 145);">MEKWMS</span></h1>
+                    <p style="text-align: justify;"><span style="font-size: 14pt;"><br>As a pioneer in logistics
+                            management solutions (WMS) for <span
+                                style="font-family: arial, helvetica, sans-serif;">3</span>PLs, Meksmart always aims at
+                            the modernity and intelligence of technology to provide the most optimal solutions. Partner
+                            with us to rapidly transform your paper-based, error-prone, complex operations, …and become
+                            modernized service leaders that can focus on satisfaction customer satisfaction, increase
+                            the efficiency of logistics operations and contribute to the rapid growth of the business.
+                            Efficient management of logistics operations. Integrates easily with technology platforms,
+                            shopping carts, retailers and shipper connections. Eliminate manual errors, inefficiencies
+                            and improve inventory management reporting.</span></p>
+                    <div class="flex mt-12 pb-10 pr-24 text-center w-full"><a
+                            href="https://meksmart.com/vi/solutions/mek-wms-3pls"
+                            class="btn-blue cursor-pointer sm:w-1/2 sm:pr-24">Read more</a></div>
+                </div>
+                <div class="flex image-content items-center lg:pl-6 lg:w-1/2 w-full"><img
+                        src="https://meksmart.com/storage/shares/Hinh Anh/Hình present-35.png" class="w-full"></div>
+            </div>
+        </div>
+    </section>
+
+    <!-- -------- tinh nang --------- -->
+    <section id="tinh-nang" class="pb-20 bg-tinh-nang">
+        <div class="lg:pb-48 pb-24 bg-quat-bottom-right relative bg-white z-0"></div>
+        <div class="container mx-auto pt-40 pb-20">
+            <h2 class="raleway-bold"></h2>
+            <h1><span style="font-size: 24pt; color: rgb(0, 20, 145);"><strong>MEK- WMS OVERALL FEATURES</strong></span>
+            </h1>
+        </div>
+        <div class="flex w-full justify-start raleway-regular lg:flex-no-wrap flex-wrap">
+            <div class="item-big item-first-left item-tinh-nang hover_icon_blue_right"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-end flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-07.svg"
+                                style="width: 85px; height: auto;"></div> <span>Inventory</span>
+                    </div>
+                </a></div>
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-12.svg"
+                                style="width: 85px; height: auto;"></div> <span>User Management</span>
+                    </div>
+                </a></div>
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-11.svg"
+                                style="width: 85px; height: auto;"></div> <span>Inbound</span>
+                    </div>
+                </a></div>
+        </div>
+        <div class="flex w-full justify-end lg:flex-no-wrap flex-wrap">
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-08.svg"
+                                style="width: 85px; height: auto;"></div> <span>Report</span>
+                    </div>
+                </a></div>
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-09.svg"
+                                style="width: 85px; height: auto;"></div> <span>Other Modules</span>
+                    </div>
+                </a></div>
+            <div class="item-big item-tinh-nang item-first-right hover_icon_blue_left"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-start flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-10.svg"
+                                style="width: 85px; height: auto;"></div> <span>Outbound</span>
+                    </div>
+                </a></div>
+        </div>
+        <div class="container mx-auto pt-40 pb-20">
+            <h2 class="raleway-bold"></h2>
+            <h1><span style="font-size: 24pt; color: rgb(0, 20, 145);"><strong>MEK- TMS OVERALL FEATURES</strong></span>
+            </h1>
+        </div>
+        <div class="flex w-full justify-start raleway-regular lg:flex-no-wrap flex-wrap">
+            <div class="item-big item-first-left item-tinh-nang hover_icon_blue_right"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-end flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-07.svg"
+                                style="width: 85px; height: auto;"></div> <span>Order List</span>
+                    </div>
+                </a></div>
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-12.svg"
+                                style="width: 85px; height: auto;"></div> <span>User Management</span>
+                    </div>
+                </a></div>
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-11.svg"
+                                style="width: 85px; height: auto;"></div> <span>Delivery Information</span>
+                    </div>
+                </a></div>
+        </div>
+        <div class="flex w-full justify-end lg:flex-no-wrap flex-wrap">
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-08.svg"
+                                style="width: 85px; height: auto;"></div> <span>Report</span>
+                    </div>
+                </a></div>
+            <div class="item-small item-tinh-nang item-nth hover_icon_blue_all"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-center flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-09.svg"
+                                style="width: 85px; height: auto;"></div> <span>Other Modules</span>
+                    </div>
+                </a></div>
+            <div class="item-big item-tinh-nang item-first-right hover_icon_blue_left"><a
+                    href="https://meksmart.com/en/solutions" class="w-full justify-start flex">
+                    <div class="flex justify-center flex-col items-center">
+                        <div class="w-full flex items-center justify-center" style="width: 90px; height: 90px;"><img
+                                src="https://meksmart.com/storage/shares/IMG_HOME/Icon/icon-10.svg"
+                                style="width: 85px; height: auto;"></div> <span>Delivery Information</span>
+                    </div>
+                </a></div>
+        </div>
+    </section>
+</template>
